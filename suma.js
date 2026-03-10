347cdd8dec76ce43b687750f7b503de4a15f70f1
GioPadilla <gioimanol17@gmail.com>
2026-03-10 02:24:15 -0600

    Mi primer commit

---
+++ suma.js
@@ -0,0 +1,18 @@
+var numero1 = parseInt( prompt("Ingresa numero 1"));
+var numero2 = parseInt(prompt("Ingresa numero 2"));
+
+
+var suma = numero1+numero2;
+console.log("La suma de: "+numero1+"+"+numero2+"="+suma);
+
+var resta = numero1-numero2;
+console.log("La resta es:"+numero1+"-"+numero2+"="+resta);
+
+var multiplicacion = numero1 * numero2;
+console.log("La multiplicacion es:"+numero1+"*"+numero2+"="+multiplicacion);
+
+var division = numero1 / numero2;
+console.log("La division es:"+numero1+"/"+numero2+"="+division);
+
+var modulo = numero1 % numero2;
+console.log("El modulo es:"+numero1+"%"+numero2+"="+modulo);
