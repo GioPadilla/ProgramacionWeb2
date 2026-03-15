7f73a2f8c167432aeca0ab6b78f43a014ab54361
GioPadilla <gioimanol17@gmail.com>
2026-03-15 01:33:05 -0600

    Add files via upload

---
+++ Operalogicas.js
@@ -0,0 +1,17 @@
+var compara = 0 && 0;
+console.log(compara);
+var compara = 0 && 1;
+console.log(compara);
+var compara = 1 && 0;
+console.log(compara);
+var compara = 1 && 1;
+console.log(compara);
+
+var comnpara = 0||0;
+console.log(compara);
+var comnpara = 0||1;
+console.log(compara);
+var comnpara = 1||0;
+console.log(compara);
+var comnpara = 1||1;
+console.log(compara);
