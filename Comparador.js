96800830e4e04f92bfddc477cbf0159228f0248e
GioPadilla <gioimanol17@gmail.com>
2026-03-15 02:45:14 -0600

    Add files via upload

---
+++ Comparador.js
@@ -0,0 +1,8 @@
+var Número = parseInt(prompt("Ingresa un número del uno al diez"));
+if(Número <= 10)
+{
+console.log("Si es un numero del 1 al 10");
+} else
+{
+console.log("Sobre pasaste el rango");
+}
